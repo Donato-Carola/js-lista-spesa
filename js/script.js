9c4c9685ff5325b3aaed944ec4b64236a7b43e20
Donato-Carola <don94@live.it>
2023-11-17 17:54:42 +0100

    prima prova sull'esecuzione dell'esercizio

--- js/script.js
+++ js/script.js
@@ -2,3 +2,32 @@
 //Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) 
 //gli elementi della lista individualmente con un ciclo while.
 
+
+const elementDiv = document.createElement('div');
+const body=document.querySelector('body');
+body.appendChild(elementDiv);
+
+const h1 = document.createElement('h1');
+elementDiv.appendChild(h1);
+
+const callH1 = document.querySelector('h1');
+
+const listaSpesa= [' 100kg bistecche', ' 100 birre', ' 100g di sale', '50 arance', '50kg riso', '50kg di kiwi'];
+
+let prodotto = 0;
+
+let i = 0;
+
+while(i<listaSpesa.length){
+  
+   prodotto = listaSpesa[i];
+   console.log(i,prodotto);
+   i++
+   
+
+    
+    
+}
+
+//console.log(i,prodotto)
+callH1.innerHTML=`${prodotto}`;
